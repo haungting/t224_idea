bae24ee4c8d8cf5ae97aa100528285518dfbab27
haungting <2693394420@qq.com>
2019-10-17 21:24:47 +0800

    htidea提交

--- gitssm/src/com/huangting/Dome.java
+++ gitssm/src/com/huangting/Dome.java
@@ -8,6 +8,6 @@ package com.huangting;
 public class Dome {
     public static void main(String[] args) {
         System.out.print("git ideaui test update");
-        System.out.print("常记溪亭日暮");
+        System.out.print("常记溪亭日暮1");
     }
 }
