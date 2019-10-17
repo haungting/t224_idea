2eea29854b288e09d006280d9bbe388ba0af726b
haungting <2693394420@qq.com>
2019-10-17 20:42:22 +0800

    git send local repository

---
+++ gitssm/src/com/huangting/Dome.java
@@ -0,0 +1,12 @@
+package com.huangting;
+
+/**
+ * @author 黄大娘
+ * @company dogsun
+ * @oreata 2019-10-17 20:33
+ */
+public class Dome {
+    public static void main(String[] args) {
+        System.out.print("git ideaui test");
+    }
+}
